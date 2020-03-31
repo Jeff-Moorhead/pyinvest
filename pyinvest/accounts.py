8dfe9d67d7cb1f27eede035c5b62fa243499df85
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-03-30 21:20:49 -0400

    Implement order placing and canceling functionality

--- pyinvest/accounts.py
+++ pyinvest/accounts.py
@@ -3,6 +3,8 @@ from pyinvest import auth
 
 """
 Significant keys: positions, unsettledCash, cashAvailableForTrading
+
+This would be better to refactor into a class. Look into the requests.Session class.
 """
 
 
@@ -10,8 +12,8 @@ def get_account_info(account, access_token):
     URL = f"https://api.tdameritrade.com/v1/accounts/{account}"
     headers = {'Authorization': f'Bearer {access_token}'}
     params = {'fields': 'positions'}
-    response = requests.get(URL, headers=headers, params=params)
-    
+
+    response = requests.get(URL, headers=headers, params=params) 
     response.raise_for_status()
 
     return response.json()['securitiesAccount']
@@ -19,4 +21,45 @@ def get_account_info(account, access_token):
 
 def get_positions(account, access_token):
     return get_account_info(account, access_token)['positions']
+
+
+def place_order(symbol, quantity, account, access_token):
+    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    params = {
+        "orderType": "MARKET",
+        "session": "NORMAL",
+        "duration": "DAY",
+        "orderStrategyType": "SINGLE",
+        "orderLegCollection": [
+            {
+                "instruction": "Buy",
+                "quantity": quantity,
+                "instrument": {
+                    "symbol": f"{symbol}",
+                    "assetType": "EQUITY"
+                }
+            }
+        ]
+    }
+    response = requests.post(URL, headers=headers, json=params)
+
+    return response.status_code
+
+
+def cancel_order(orderid, account, access_token):
+    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders/{orderid}"
+    headers = {'Authorization': f'Bearer {access_token}'} 
+    response = requests.delete(URL, headers=headers)
+
+    return response.status_code
+ 
+
+def get_orders(account, access_token):
+    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders"
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(URL, headers=headers)
+    response.raise_for_status()
+    
+    return response.json()
 
