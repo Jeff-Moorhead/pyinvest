a6fc32a4c6511e830b60f06c3aaa2b5507a9014c
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-03-31 22:13:36 -0400

    Remove unneeded config methods

--- pyinvest/config.py
+++ pyinvest/config.py
@@ -9,15 +9,6 @@ class Config:
         self.client_id = self._data['client_id']
         self.account_id = self._data['account_id']
 
-    def _get_refresh_token(self):
-        return self.authentication_data['refresh_token']
-
-    def _get_authentication_data(self):
-        return {'refresh_token': self.data['refresh_token'], 'client_id': self.data['client_id']}
-
-    def _get_account_id(self):
-        return self.data['account_id']
-
     def _get_config_data(self, configpath):
         return json.load(open(os.path.expandvars(configpath)))
 
