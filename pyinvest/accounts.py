a0b2c3d99ebeaabebc76b6dd96e2b02e48a740df
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-03-31 21:41:49 -0400

    Refactored accounts and config. Added functionality to tdapositions script

--- pyinvest/accounts.py
+++ pyinvest/accounts.py
@@ -1,5 +1,4 @@
 import requests
-from pyinvest import auth
 
 """
 Significant keys: positions, unsettledCash, cashAvailableForTrading
@@ -8,58 +7,61 @@ This would be better to refactor into a class. Look into the requests.Session cl
 """
 
 
-def get_account_info(account, access_token):
-    URL = f"https://api.tdameritrade.com/v1/accounts/{account}"
-    headers = {'Authorization': f'Bearer {access_token}'}
-    params = {'fields': 'positions'}
-
-    response = requests.get(URL, headers=headers, params=params) 
-    response.raise_for_status()
-
-    return response.json()['securitiesAccount']
+def calculate_total_profit(shares, avg_cost, current_price):
+    return round((current_price - avg_cost) * shares, 2)
 
 
-def get_positions(account, access_token):
-    return get_account_info(account, access_token)['positions']
+def calculate_total_profit_percentage(shares, avg_cost, current_price):
+    pcnt = ((shares * current_price) - (shares * avg_cost)) / (shares * avg_cost)
+    return round(pcnt * 100, 2)
 
 
-def place_order(symbol, quantity, account, access_token):
-    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders"
-    headers = {'Authorization': f'Bearer {access_token}'}
-    params = {
-        "orderType": "MARKET",
-        "session": "NORMAL",
-        "duration": "DAY",
-        "orderStrategyType": "SINGLE",
-        "orderLegCollection": [
-            {
-                "instruction": "Buy",
-                "quantity": quantity,
-                "instrument": {
-                    "symbol": f"{symbol}",
-                    "assetType": "EQUITY"
+class TDAmeritradeClient:
+    def __init__(self, access_token, account):
+        self.account = account
+        self.session = requests.Session()
+        self.session.headers = {'Authorization': f'Bearer {access_token}'}
+
+    def get_account_info(self):
+        URL = f"https://api.tdameritrade.com/v1/accounts/{self.account}"
+        params = {'fields': 'positions'}
+
+        response = self.session.get(URL, params=params) 
+        response.raise_for_status()
+        return response.json()['securitiesAccount']
+
+    def get_positions(self):
+        return self.get_account_info()['positions']
+
+    def place_order(self, symbol, quantity):
+        URL = f"https://api.tdameritrade.com/v1/accounts/{self.account}/orders"
+        params = {
+            "orderType": "MARKET",
+            "session": "NORMAL",
+            "duration": "DAY",
+            "orderStrategyType": "SINGLE",
+            "orderLegCollection": [
+                {
+                    "instruction": "Buy",
+                    "quantity": quantity,
+                    "instrument": {
+                        "symbol": f"{symbol}",
+                        "assetType": "EQUITY"
+                    }
                 }
-            }
-        ]
-    }
-    response = requests.post(URL, headers=headers, json=params)
+            ]
+        }
+        response = self.session.post(URL, json=params)
+        return response.status_code
 
-    return response.status_code
+    def cancel_order(self, orderid):
+        URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders/{orderid}"
+        response = self.session.delete(URL)
+        return response.status_code
 
-
-def cancel_order(orderid, account, access_token):
-    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders/{orderid}"
-    headers = {'Authorization': f'Bearer {access_token}'} 
-    response = requests.delete(URL, headers=headers)
-
-    return response.status_code
- 
-
-def get_orders(account, access_token):
-    URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders"
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(URL, headers=headers)
-    response.raise_for_status()
-    
-    return response.json()
+    def get_orders(self):
+        URL = f"https://api.tdameritrade.com/v1/accounts/{account}/orders"
+        response = self.session.get(URL)
+        response.raise_for_status()
+        return response.json()
 
--- pyinvest/config.py
+++ pyinvest/config.py
@@ -2,16 +2,22 @@ import os
 import json
 
 
-def get_authentication_data(configpath):
-    data = _get_config_data(configpath)
-    return {'refresh_token': data['refresh_token'], 'client_id': data['client_id']}
+class Config:
+    def __init__(self, configpath):
+        self._data = self._get_config_data(configpath)
+        self.refresh_token = self._data['refresh_token']
+        self.client_id = self._data['client_id']
+        self.account_id = self._data['account_id']
 
+    def _get_refresh_token(self):
+        return self.authentication_data['refresh_token']
 
-def get_account_data(configpath):
-    data = _get_config_data(configpath)
-    return {'account_id': data['account_id']}
+    def _get_authentication_data(self):
+        return {'refresh_token': self.data['refresh_token'], 'client_id': self.data['client_id']}
 
+    def _get_account_id(self):
+        return self.data['account_id']
 
-def _get_config_data(configpath):
-    return json.load(open(os.path.expandvars(configpath)))
+    def _get_config_data(self, configpath):
+        return json.load(open(os.path.expandvars(configpath)))
 
