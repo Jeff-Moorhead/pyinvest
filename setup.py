2fb4c6b2ee3f5d0f5287d8b657f143590ad73913
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-04-06 21:54:48 -0400

    Add selenium to requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PyInvest',
     version='1.0',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=['requests', 'selenium'],
     author='Jeff Moorhead',
     author_email='Jeff.moorhead1@gmail.com',
     description='A Python wrapper for the TD Ameritrade REST API',
