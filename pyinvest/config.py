b8217963e8a4f3bff88e675eb2e0c9f95c94f9aa
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-04-08 21:42:03 -0400

    Move allocations to config. Provide config template file

--- pyinvest/config.py
+++ pyinvest/config.py
@@ -9,6 +9,7 @@ class Config:
         self.username = self._data['username']
         self.account_id = self._data['account_id']
         self.redirect = self._data['redirect']
+        self.allocations = self._data['allocations']
 
     def _get_config_data(self, configpath):
         return json.load(open(os.path.expandvars(configpath)))
