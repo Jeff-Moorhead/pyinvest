6f717bc9fb9462361bb905e9bcf0225be5aa975c
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-07-16 15:19:23 -0400

    Add MANIFEST.in and automatically install package data

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyInvest',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
+    data_files=[("pyinvestconf", ['conf/pyinvest.json'])],
     install_requires=['requests', 'selenium'],
     author='Jeff Moorhead',
     author_email='Jeff.moorhead1@gmail.com',
